20191783654bb339f00e76920724dfce4f1d6e42
fly-man- <fly.man.opensim@gmail.com>
2018-09-18 15:34:39 +0200

    Adding Dutch language files

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,21 @@
+<?php
+$lang['ColorPalette'] = 'Kleur Palet';
+$lang['Color Palette'] = 'Kleur Palet';
+$lang['Palette'] = 'Palet';
+$lang['Palette Search'] = 'Zoek Palet';
+$lang['Number of colors'] = 'Aantal kleuren';
+$lang['Number of colors in palette (default %d)'] = 'Aantal kleuren in palet (standaard %d)';
+$lang['Number of colors must have a value between %d and %d'] = 'Aantal kleuren moet een waarde hebben tussen %d en %d';
+$lang['Sample image size'] = 'Sample image grootte';
+$lang['Sample image size for palette generation (default %d px)'] = 'Sample image grootte voor palet generatie (standaard %d px)';
+$lang['Clear all palettes'] = 'Verwijder alle paletten';
+$lang['Clear color palette'] = 'Verwijder kleur palet';
+$lang['Generate color palette'] = 'Genereer kleur palet';
+$lang['With color palette'] = 'Met kleur palet';
+$lang['Without color palette'] = 'Zonder kleur palet';
+$lang['Generate palette on image page'] = 'Genereer palette op image pagina';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'Als een palet nog niet gegeneerd is voor deze image wordt dit automatisch gedaan als de image geopend wordt';
+$lang['Unable to process %s'] = 'Niet mogelijk te verwerken %s';
+$lang['Operation successfully completed'] = 'De bewerking is succesvol voltooid';
+$lang['Configuration successfully updated'] = 'Configuratie is succesvol bijgewerkt';
+?>
