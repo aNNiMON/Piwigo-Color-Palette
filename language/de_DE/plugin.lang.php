17a0184bc6ebafb542276b2e0eea601e7f51bde2
fly-man- <fly.man.opensim@gmail.com>
2018-09-18 15:34:25 +0200

    Adding German language files

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,21 @@
+<?php
+$lang['ColorPalette'] = 'Farbpalette';
+$lang['Color Palette'] = 'Farbpalette';
+$lang['Palette'] = 'Palette';
+$lang['Palette Search'] = 'Palette Suchen';
+$lang['Number of colors'] = 'Anzahl der Farben';
+$lang['Number of colors in palette (default %d)'] = 'Anzahl der Farben in der Palette (standard %d)';
+$lang['Number of colors must have a value between %d and %d'] = 'Anzahl der Farben muss einen Wert haben zwischen %d und %d';
+$lang['Sample image size'] = 'Beispielbildgröße';
+$lang['Sample image size for palette generation (default %d px)'] = 'Beispielbildgröße für die Palettengenerierung (standard %d px)';
+$lang['Clear all palettes'] = 'Alle Paletten löschen';
+$lang['Clear color palette'] = 'Klare Farbpalette';
+$lang['Generate color palette'] = 'Generieren die Farbpalette';
+$lang['With color palette'] = 'Mit farbe palette';
+$lang['Without color palette'] = 'Ohne farbe palette';
+$lang['Generate palette on image page'] = 'Palette auf Bildseite generieren';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'Wenn für das Bild noch keine Palette erstellt wurde, wird dies beim ersten Öffnen des Bildes durchgeführt';
+$lang['Unable to process %s'] = 'Anfrage kann nicht bearbeitet werden %s';
+$lang['Operation successfully completed'] = 'Die Operation wurde erfolgreich abgeschlossen';
+$lang['Configuration successfully updated'] = 'Die Konfiguration wurde erfolgreich aktualisiert';
+?>
