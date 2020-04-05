49a3d6b1c7f1fec1ba0c76ec40d8af9d01ce3a89
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-05 21:06:46 +0200

    Update fr_FR, thanks to laurent.duretz

--- language/fr_FR/plugin.lang.php
+++ language/fr_FR/plugin.lang.php
@@ -6,3 +6,25 @@
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
 $lang['Number of colors'] = 'nombre de couleur';
+$lang['Sample image coverage'] = 'Image de démo de couverture';
+$lang['Sample image coverage from center (default 100% - full image)'] = 'Image de démo de couverture depuis le centre (100% par défaut - image complète)';
+$lang['Sample image coverage must have a value between %d and %d'] = 'La couverture de l\'image démo doit être comprise entre %d et %d';
+$lang['Sample image must have a size between %d and %d'] = 'L\'image de démo doit avoir une taille entre %d et %d';
+$lang['Sample image size'] = 'Taille de l\'image de démo';
+$lang['Sample image size for palette generation (default %d px)'] = 'Taille de l\'image de démo pour la génération de la palette (%d px par défaut)';
+$lang['With color palette'] = 'Avec une palette de couleur';
+$lang['Without color palette'] = 'Sans palette de couleur';
+$lang['Unable to process %s'] = 'Impossible d\'exécuter %s';
+$lang['Palette Search'] = 'Chercher une palette';
+$lang['Palette'] = 'Palette';
+$lang['Operation successfully completed'] = 'Opération effectuée avec succès';
+$lang['Number of colors in palette (default %d)'] = 'Nombre de couleurs de la palette (%d par défaut)';
+$lang['Number of colors must have a value between %d and %d'] = 'Le nombre de couleurs est une valeur entre %d et %d';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'Si la palette n\'est pas encore générée pour l\'images, cela sera fait au premier affichage de celle-ci';
+$lang['Clear all palettes'] = 'Vider toutes les palettes';
+$lang['Clear color palette'] = 'Vider la palette';
+$lang['Generate palette on image page'] = 'Générer la palette sur la page de l\'image';
+$lang['Generate color palette'] = 'Générer la palette de couleur';
+$lang['Configuration successfully updated'] = 'Configuration mise à jour avec succès';
+$lang['ColorPalette'] = 'Palette de couleur';
+$lang['Color Palette'] = 'Palette de couleur';
