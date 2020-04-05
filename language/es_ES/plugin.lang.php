4c0eb272c6a16ea65dfb9d2c1d3df12981a3be22
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-05 21:07:42 +0200

    Add es_ES, thanks to Tirs

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Clear all palettes'] = 'Borrar todas las paletas';
+$lang['Clear color palette'] = 'Borrar la paleta de colores';
+$lang['Color Palette'] = 'Paleta de colores';
+$lang['ColorPalette'] = 'Paleta de colores';
+$lang['Configuration successfully updated'] = 'La configuración se ha actualizado con éxito';
+$lang['Generate color palette'] = 'Generar paleta de colores';
+$lang['Generate palette on image page'] = 'Generar paleta en la página de imagen';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'Si la paleta para la imagen no se ha generado aún, se generará la primera vez que se abra la imagen';
+$lang['Number of colors'] = 'Número de colores';
+$lang['Number of colors in palette (default %d)'] = 'Número de colores de la paleta (por defecto %d)';
+$lang['Number of colors must have a value between %d and %d'] = 'El número de colores debe tener un valor entre %d y %d';
+$lang['Operation successfully completed'] = 'La operación se ha completado con éxito';
+$lang['Palette'] = 'Paleta';
+$lang['Palette Search'] = 'Búsqueda de paletas';
+$lang['Sample image coverage'] = 'Cobertura de imagen de muestra';
+$lang['Sample image coverage from center (default 100% - full image)'] = 'Cobertura de imagen de muestra desde el centro (por defecto 100% - toda la imagen)';
+$lang['Sample image coverage must have a value between %d and %d'] = 'La cobertura de imagen de muestra tiene que tener un valor entre %d y %d';
+$lang['Sample image must have a size between %d and %d'] = 'La imagen de muestra tiene que tener un tamaño entre %d y %d';
+$lang['Sample image size'] = 'Tamaño de la imagen de muestra';
+$lang['Sample image size for palette generation (default %d px)'] = 'Tamaño de la imagen de muestra para la generación de la paleta (por defecto %d píxeles)';
+$lang['Unable to process %s'] = 'No se puede procesar %s';
+$lang['With color palette'] = 'Con paleta de colores';
+$lang['Without color palette'] = 'Sin paleta de colores';
