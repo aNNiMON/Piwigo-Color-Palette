df3ff04c16c62bf9ee034d5b683f3673353dd3a3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-05 21:07:00 +0200

    Add it_IT, thanks to mclee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Without color palette'] = 'Senza la tavolozza dei colori';
+$lang['With color palette'] = 'Con la tavolozza dei colori';
+$lang['Unable to process %s'] = 'Impossibile elaborare %s';
+$lang['Sample image must have a size between %d and %d'] = 'L\'immagine di esempio deve avere una dimensione compresa tra %d e %d';
+$lang['Sample image coverage must have a value between %d and %d'] = 'La copertura dell\'immagine di esempio deve avere un valore compreso tra %d e %d';
+$lang['Sample image coverage from center (default 100% - full image)'] = 'Copertura dell\'immagine di esempio dal centro (impostazione predefinita 100% - immagine intera)';
+$lang['Sample image coverage'] = '21/5000
+Copertura dell\'immagine di esempio';
+$lang['Palette Search'] = 'Ricerca tavolozza';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'Se la tavolozza non è ancora stata generata per l\'immagine, ciò verrà fatto alla prima apertura dell\'immagine';
+$lang['Generate palette on image page'] = 'Genera tavolozza sulla pagina dell\'immagine';
+$lang['Generate color palette'] = 'Genera tavolozza dei colori';
+$lang['Configuration successfully updated'] = 'La configurazione è stata aggiornata con successo';
+$lang['ColorPalette'] = 'Tavolozza di colori';
+$lang['Color Palette'] = 'Tavolozza di colori';
+$lang['Clear color palette'] = 'Cancella tavolozza dei colori';
+$lang['Clear all palettes'] = 'Cancella tutte le tavolozze';
+$lang['Number of colors must have a value between %d and %d'] = 'Il numero di colori deve avere un valore compreso fra %d e %d';
+$lang['Operation successfully completed'] = 'Operazione completata';
+$lang['Sample image size for palette generation (default %d px)'] = 'Dimensione dell\'immagine di esempio per la generazione della tavolozza (default %d px)';
+$lang['Sample image size'] = 'Dimensione dell\'immagine di esempio';
+$lang['Palette'] = 'Tavolozza';
+$lang['Number of colors in palette (default %d)'] = 'Numero di colori nella tavolozza (default %d)';
+$lang['Number of colors'] = 'Numero di colori';
