e59e5f0e5bce6c3d7d5a15a63d46102162bd74b8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-08 20:57:44 +0200

    Add lv_LV, thanks to agrisans

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'Ja attēlam vēl nav ģenerēta palete, tas tiks paveikts pirmo reizi atverot attēlu';
+$lang['Sample image size for palette generation (default %d px)'] = 'Parauga attēla izmērs paletes ģenerēšanai (pēc noklusējuma %d px)';
+$lang['Sample image must have a size between %d and %d'] = 'Parauga attēlam jābūt izmērā no %d līdz %d';
+$lang['Sample image coverage from center (default 100% - full image)'] = 'Parauga attēla pārklājums no centra (noklusētā vērtība 100% - pilns attēls)';
+$lang['Sample image coverage must have a value between %d and %d'] = 'Parauga attēla pārklājumam jābūt vērtībā starp %d un %d';
+$lang['Number of colors must have a value between %d and %d'] = 'Krāsu skaitam jābūt vērtībā starp %d un %d';
+$lang['Configuration successfully updated'] = 'Konfigurācija veiksmīgi atjaunināta';
+$lang['Operation successfully completed'] = 'Operācija veiksmīgi pabeigta';
+$lang['Unable to process %s'] = 'Nav iespējams veikt %s';
+$lang['With color palette'] = 'Ar krāsu paleti';
+$lang['Without color palette'] = 'Bez krāsu paletes';
+$lang['Sample image coverage'] = 'Parauga attēla pārklājums';
+$lang['Sample image size'] = 'Parauga attēla izmērs';
+$lang['Number of colors in palette (default %d)'] = 'Krāsu skaits paletē (pēc noklusējuma %d) ';
+$lang['Palette Search'] = 'Paletes meklēšana';
+$lang['Palette'] = 'Palete';
+$lang['Number of colors'] = 'Krāsu skaits';
+$lang['Generate palette on image page'] = 'Ģenerēt paleti attēlu lapā';
+$lang['Generate color palette'] = 'Ģenerēt krāsu paleti';
+$lang['ColorPalette'] = 'Krāsu Palete';
+$lang['Color Palette'] = 'Krāsu Palete';
+$lang['Clear color palette'] = 'Notīrīt krāsu paletes';
+$lang['Clear all palettes'] = 'Notīrīt visas paletes';
