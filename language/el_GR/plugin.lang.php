16b7ce5a7c59f0696860984f25c6cbdc6253497b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-08-07 13:27:24 +0200

    Add el_GR, thanks to bas_alba

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Generate palette on image page'] = 'Δημιουργήστε παλέτα στη σελίδα εικόνας';
+$lang['Number of colors'] = 'Αριθμός χρωμάτων';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'Εάν η παλέτα δεν έχει δημιουργηθεί ακόμη για την εικόνα, αυτό θα γίνει την πρώτη φορά που θα ανοίξει η εικόνα';
+$lang['Generate color palette'] = 'Δημιουργήστε παλέτα χρωμάτων';
+$lang['Configuration successfully updated'] = 'Η διαμόρφωση ενημερώθηκε με επιτυχία';
+$lang['ColorPalette'] = 'Παλέτα χρωμάτων';
+$lang['Color Palette'] = 'Παλέτα χρωμάτων';
+$lang['Clear color palette'] = 'Καθαρισμός Χρωματική Παλέτας';
+$lang['Clear all palettes'] = 'Διαγραφή όλων των παλετών';
