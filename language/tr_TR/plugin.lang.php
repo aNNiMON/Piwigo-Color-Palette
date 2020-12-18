5bbbbe51343f57d5c3927c6bb70d27ef35f495ef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-12-18 18:24:19 +0100

    Add tr_TR, thanks to LazBoy

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'Eğer imaj için henüz palet oluşturulmadıysa, bu işlem imajın ilk açılması esnasında yapılacaktır';
+$lang['Number of colors must have a value between %d and %d'] = 'Renk sayısı %d ile %d arasında bir değer olmalı';
+$lang['Sample image coverage must have a value between %d and %d'] = 'Örnek imaj kapsamı %d ile %d arasında bir değer olmalı';
+$lang['Sample image coverage from center (default 100% - full image)'] = 'Merkezden örnek görüntü kapsamı (varsayılan 100% - tam imaj)';
+$lang['Sample image coverage'] = 'Örnek imaj kapsamı';
+$lang['Operation successfully completed'] = 'Operasyon başarıyla tamamlandı';
+$lang['Number of colors in palette (default %d)'] = 'Paletteki renk sayısı(varsayılan %d)';
+$lang['Number of colors'] = 'Renk sayısı';
+$lang['Generate palette on image page'] = 'İmaj sayfasında palet oluştur';
+$lang['Generate color palette'] = 'Renk paleti oluştur';
+$lang['Configuration successfully updated'] = 'Yapılandırma başarıyla güncellendi';
+$lang['ColorPalette'] = 'Renk Paleti';
+$lang['Color Palette'] = 'Renk Paleti';
+$lang['Clear color palette'] = 'Renk paletini temizle';
+$lang['Clear all palettes'] = 'Tüm paletleri temizle';
+$lang['Palette'] = 'Palet';
+$lang['Palette Search'] = 'Palet arama';
+$lang['Sample image must have a size between %d and %d'] = 'Örnek imaj büyüklüğü %d ile %d arasında olmalı';
+$lang['Sample image size'] = 'Örnek imaj büyüklüğü';
+$lang['Sample image size for palette generation (default %d px)'] = 'Palet oluşturulması için örnek imaj büyüklüğü (varsayılan %d px)';
+$lang['With color palette'] = 'Renk paleti ile';
+$lang['Without color palette'] = 'Renk paleti olmadan';
+$lang['Unable to process %s'] = '%s işlemi yapılamıyor';
