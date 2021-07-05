737c6a83c6b59cf3fe5123d7849be79639d71af8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-07-05 16:52:43 +0200

    Update sk_SK, thanks to jose1711

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'Ak pre obrázok nebola vygenerovaná paleta bude vygenerovaná pri prvom otvorení obrázka.';
+$lang['Unable to process %s'] = 'Nepodarilo sa spracovať %s';
+$lang['With color palette'] = 'S paletou farieb';
+$lang['Without color palette'] = 'Bez palety farieb';
+$lang['Clear all palettes'] = 'Vyčistiť všetky palety';
+$lang['Clear color palette'] = 'Vyčistiť paletu farieb';
+$lang['Color Palette'] = 'Paleta farieb';
+$lang['ColorPalette'] = 'Paleta farieb';
+$lang['Configuration successfully updated'] = 'Nastavenia bolo úspešne aktualizované';
+$lang['Generate color palette'] = 'Generovať paletu farieb';
+$lang['Generate palette on image page'] = 'Generovať paletu na stránke s obrázkom';
+$lang['Number of colors'] = 'Počet farieb';
+$lang['Number of colors in palette (default %d)'] = 'Počet farieb palety (predvolene %d)';
+$lang['Number of colors must have a value between %d and %d'] = 'Počet farieb musí byť hodnota medzi %d a %d';
+$lang['Operation successfully completed'] = 'Akcia bola úspešne dokončená';
+$lang['Palette'] = 'Paleta';
+$lang['Palette Search'] = 'Hľadanie v palete';
