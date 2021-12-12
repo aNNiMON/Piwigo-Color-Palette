c3b57becb8560a12b07fa18ed963ddd0bcb417fb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-12 20:23:13 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Configuration successfully updated'] = 'Konfiguracija je bila uspešno posodobljena';
+$lang['Operation successfully completed'] = 'Operacija je bila uspešno zaključena';
+$lang['Unable to process %s'] = '%s ni mogoče obdelati';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'Če paleta še ni ustvarjena za sliko, bo to storjeno ob prvem odprtju slike';
+$lang['Generate palette on image page'] = 'Ustvari paleto na strani s sliko';
+$lang['With color palette'] = 'Z barvno paleto';
+$lang['Without color palette'] = 'Brez barvne palete';
+$lang['Clear all palettes'] = 'Počisti vse palete';
+$lang['Clear color palette'] = 'Jasna barvna paleta';
+$lang['Generate color palette'] = 'Ustvari barvno paleto';
+$lang['Sample image coverage must have a value between %d and %d'] = 'Pokritost vzorčne slike mora imeti vrednost med %d in %d';
+$lang['Sample image coverage from center (default 100% - full image)'] = 'Pokritost vzorčne slike od sredine (privzeto 100% - celotna slika)';
+$lang['Sample image coverage'] = 'Pokritost vzorčne slike';
+$lang['Sample image must have a size between %d and %d'] = 'Velikost vzorčne slike mora biti med %d in %d';
+$lang['Sample image size for palette generation (default %d px)'] = 'Velikost vzorčne slike za ustvarjanje palete (privzeto %d px)';
+$lang['Sample image size'] = 'Velikost vzorčne slike';
+$lang['Number of colors must have a value between %d and %d'] = 'Število barv mora imeti vrednost med %d in %d';
+$lang['Number of colors in palette (default %d)'] = 'Število barv v paleti (privzeto %d)';
+$lang['Number of colors'] = 'Število barv';
+$lang['Palette Search'] = 'Iskanje po paleti';
+$lang['Palette'] = 'Paleta';
+$lang['Color Palette'] = 'Barvnapaleta';
+$lang['ColorPalette'] = 'Barvnapaleta';
