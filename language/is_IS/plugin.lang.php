6476a40d3b772fa74560b69faaf6e941d71038f6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-11 05:52:48 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Unable to process %s'] = 'Ekki hægt að vinna úr %s';
+$lang['With color palette'] = 'Með litatöflu';
+$lang['Without color palette'] = 'Án litatöflu';
+$lang['Clear all palettes'] = 'Hreinsaðu allar litatöflur';
+$lang['Clear color palette'] = 'Hreinsaðu litatöflu';
+$lang['Color Palette'] = 'Litatafla';
+$lang['ColorPalette'] = 'Litatafla';
+$lang['Configuration successfully updated'] = 'Stillingar hafa verið uppfærðar';
+$lang['Generate color palette'] = 'Búðu til litatöflu';
+$lang['Generate palette on image page'] = 'Búðu til litatöflu á myndasíðu';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'Ef litatafla hefur ekki verið búin til fyrir myndina verður þetta gert í fyrsta skipti sem myndin er opnuð';
+$lang['Number of colors'] = 'Fjöldi lita';
+$lang['Number of colors in palette (default %d)'] = 'Fjöldi lita í litatöflu (sjálfgefið %d)';
+$lang['Number of colors must have a value between %d and %d'] = 'Fjöldi lita verður að hafa gildi á milli %d og %d';
+$lang['Operation successfully completed'] = 'Aðgerð hefur verið lokið';
+$lang['Palette'] = 'Litatafla';
+$lang['Palette Search'] = 'Litatöfluleit';
+$lang['Sample image coverage'] = 'Dekkun sýnishornamyndar';
+$lang['Sample image coverage from center (default 100% - full image)'] = 'Dekkun sýnishornamyndar frá miðju (sjálfgefið 100% - heildarmynd)';
+$lang['Sample image coverage must have a value between %d and %d'] = 'Dekkun sýnishornamyndar verður að hafa gildi á milli %d og %d';
+$lang['Sample image must have a size between %d and %d'] = 'Stærð sýnishornamyndar verður að vera á milli %d og %d';
+$lang['Sample image size'] = 'Stærð sýnishornamyndar';
+$lang['Sample image size for palette generation (default %d px)'] = 'Stærð sýnishornamyndar fyrir að útbúa litatöflu (sjálfgefið %d px)';
