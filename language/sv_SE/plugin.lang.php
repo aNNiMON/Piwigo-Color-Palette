6e2217547362fb82d2ca891d1177fdbf8e439222
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-09-06 22:45:15 +0200

    Add sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Unable to process %s'] = 'Kan inte bearbeta %s';
+$lang['With color palette'] = 'Med färgpalett';
+$lang['Without color palette'] = 'Utan färgpalett';
+$lang['Clear all palettes'] = 'Rensa alla paletter';
+$lang['Clear color palette'] = 'Rensa färgpalett';
+$lang['Color Palette'] = 'Färgpalett';
+$lang['ColorPalette'] = 'Färgpalett';
+$lang['Configuration successfully updated'] = 'Konfigurationen har uppdaterats';
+$lang['Generate color palette'] = 'Generera färgpalett';
+$lang['Generate palette on image page'] = 'Generera färgpalett på bildsidan';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'Om palett inte ännu är genererad för bilden, kommer det att göras första gången bilden öppnas';
+$lang['Number of colors'] = 'Antal färger';
+$lang['Number of colors in palette (default %d)'] = 'Antal färger i palett (standard %d)';
+$lang['Number of colors must have a value between %d and %d'] = 'Antal färger måste ha ett värde mellan %d och %d';
+$lang['Operation successfully completed'] = 'Operationen har slutförts';
+$lang['Palette'] = 'Palett';
+$lang['Palette Search'] = 'Palettsök';
+$lang['Sample image coverage'] = 'Täckning exempelbild';
+$lang['Sample image coverage from center (default 100% - full image)'] = 'Täckning exempelbild från mitten (standard 100% - hela bilden)';
+$lang['Sample image coverage must have a value between %d and %d'] = 'Täckning exempelbild måste ha ett värde mellan %d och %d';
+$lang['Sample image must have a size between %d and %d'] = 'Exempelbilden måste ha en storlek mellan %d och %d';
+$lang['Sample image size'] = 'Bildstorlek exempelbild';
+$lang['Sample image size for palette generation (default %d px)'] = 'Bildstorlek exempelbild för palettgenerering (standard %d px)';
