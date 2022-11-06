44e18ccbfba5c911247b272f949df60180b4add9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-06 16:16:10 +0100

    Update cs_CZ, thanks to gandycz

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Unable to process %s'] = 'Nelze zpracovat %s';
+$lang['With color palette'] = 'S paletou barev';
+$lang['Without color palette'] = 'Bez palety barev';
+$lang['Clear all palettes'] = 'Vyčistit všechny palety';
+$lang['Clear color palette'] = 'Vyčistit barevnou paletu';
+$lang['Color Palette'] = 'Barevná Paleta';
+$lang['ColorPalette'] = 'Barevná Paleta';
+$lang['Configuration successfully updated'] = 'Konfigurace byla úspěšně aktualizována';
+$lang['Generate color palette'] = 'Vygenerovat barevnou paletu';
+$lang['Generate palette on image page'] = 'Generovat paletu na stránce obrázku';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'Pokud pro obrázek ještě není vygenerována paleta, provede se to při prvním otevření obrázku';
+$lang['Number of colors'] = 'Počet barev';
+$lang['Number of colors in palette (default %d)'] = 'Bočet barev v paletě (výchozí %d)';
+$lang['Number of colors must have a value between %d and %d'] = 'Počet barev musí mít hodnotu mezi %d a %d';
+$lang['Operation successfully completed'] = 'Operace byla úspěšně dokončena';
+$lang['Palette'] = 'Paleta';
+$lang['Palette Search'] = 'Hledat paletu';
+$lang['Sample image coverage'] = 'Ukázkové pokrytí obrazu';
+$lang['Sample image coverage from center (default 100% - full image)'] = 'Pokrytí ukázkového obrázku od středu (výchozí 100% – celý obrázek)';
+$lang['Sample image coverage must have a value between %d and %d'] = 'Pokrytí ukázkového obrázku musí mít hodnotu mezi %d a %d';
+$lang['Sample image must have a size between %d and %d'] = 'Ukázkový obrázek musí mít velikost mezi %d a %d';
+$lang['Sample image size'] = 'Vělikost ukázkového obrázku';
+$lang['Sample image size for palette generation (default %d px)'] = 'Ukázková velikost obrázku pro generování palety (výchozí %d px)';
