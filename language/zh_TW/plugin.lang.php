6d6e0bbef4c1e3f9c647b94954944c02caf8b27d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-07 18:03:48 +0100

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Unable to process %s'] = '無法處理 %s';
+$lang['With color palette'] = '有代表色盤';
+$lang['Without color palette'] = '沒有代表色盤';
+$lang['Clear all palettes'] = '清除所有代表色盤';
+$lang['Clear color palette'] = '清除代表色盤';
+$lang['Color Palette'] = '代表色盤';
+$lang['ColorPalette'] = '代表色盤';
+$lang['Configuration successfully updated'] = '配置參數已成功更新';
+$lang['Generate color palette'] = '產生代表色盤';
+$lang['Generate palette on image page'] = '在影像頁面產生代表色盤';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = '如果影像還沒有代表色盤的話，該影像的代表色盤會在影像第一次開啟時產生';
+$lang['Number of colors'] = '代表色數量';
+$lang['Number of colors in palette (default %d)'] = '代表色盤中的顏色數量（預設值為 %d 個）';
+$lang['Number of colors must have a value between %d and %d'] = '代表色的數量必須介於 %d 和 %d 之間';
+$lang['Operation successfully completed'] = '順利處理完成';
+$lang['Palette'] = '色盤';
+$lang['Palette Search'] = '搜尋色盤';
+$lang['Sample image coverage'] = '取樣影像的涵蓋範圍';
+$lang['Sample image coverage from center (default 100% - full image)'] = '從影像中心開始計算的取樣涵蓋範圍（預設值為 100% - 也就是整張影像）';
+$lang['Sample image coverage must have a value between %d and %d'] = '取樣影像的涵蓋範圍必須介於 %d 和 %d 之間';
+$lang['Sample image must have a size between %d and %d'] = '取樣影像的尺寸必須介於 %d 和 %d 之間';
+$lang['Sample image size'] = '取樣影像尺寸';
+$lang['Sample image size for palette generation (default %d px)'] = '產生代表色盤時所取樣的影像尺寸（預設值為 %d px）';
